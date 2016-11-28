65af13d254bd4df60656fe2dc3690651fc28022d
Corey Harrison <corey@untiltomorrow.net>
2016-11-29 08:25:25 +1000

    initial commit

--- src/home/home.controller.js
+++ src/home/home.controller.js
@@ -1,5 +1,24 @@
 class HomeController {
     constructor() {
+
+        this.buttonGroup = [
+            {
+                labelTranslationKey: "button one",
+                action: console.log('ONE')
+            },
+            {
+                labelTranslationKey: "button two",
+                action: console.log('TWO')
+            },
+            {
+                labelTranslationKey: "button three",
+                action: console.log('THREE')
+            }
+        ];
+    }
+
+    doSomething() {
+        console.log('BANG!!!')
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,23 @@ import './index.scss';
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
 import 'file?name=favicon.ico!./favicon.ico';
+import ngAria from 'angular-aria';
+import ngAnimate from 'angular-animate';
+import ngSanitize from 'angular-sanitize';
+import ngMessages from 'angular-messages';
+import ngMaterial from 'angular-material';
 
 import home from './home';
 import routesConfigurator from './index.routes.js';
 
 var dependencies = [
     uirouter,
-    home
+    home,
+    ngAria,
+    ngAnimate,
+    ngMessages,
+    ngSanitize,
+    ngMaterial
 ];
 
 angular.module('ssApp', dependencies)
